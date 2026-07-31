b63680321e2d804179a5796e121ee7b6983987d5
djorgeext <djorge.ext@fi.uba.ar>
2026-07-30 21:38:06 -0300

    defining algorithm for matching

--- NLP/Medium/Stitch-the-Torn-Wiki/stitch-the-torn-wiki-testcases/solution.py
+++ NLP/Medium/Stitch-the-Torn-Wiki/stitch-the-torn-wiki-testcases/solution.py
@@ -1,16 +1,7 @@
 from nltk.tokenize import wordpunct_tokenize
-
-# text_path = 'NLP/Medium/Stitch-the-Torn-Wiki/stitch-the-torn-wiki-testcases/input/input00.txt'
-
-# with open(text_path, 'r') as f:
-#     text = f.read().splitlines()
-
-# separator_set = text.index("*****")
-# a_set = text[1:separator_set]
-# b_set = text[separator_set+1:]
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 def text_similarity(text1, text2):
     vectorizer = TfidfVectorizer(
@@ -22,7 +13,17 @@ def text_similarity(text1, text2):
 
     return cosine_similarity(X[0], X[1])[0, 0]
 
-p1 = "Natural language processing is useful for text similarity."
-p2 = "Text similarity can be measured using natural language methods."
+text_path = 'NLP/Medium/Stitch-the-Torn-Wiki/stitch-the-torn-wiki-testcases/input/input00.txt'
 
-print(text_similarity(p1, p2))
+with open(text_path, 'r') as f:
+    text = f.read().splitlines()
+
+separator_set = text.index("*****")
+
+a_set = text[1:separator_set]
+b_set = text[separator_set+1:]
+
+
+
+for paragraph in b_set:
+    print(text_similarity(paragraph, a_set[2]))
