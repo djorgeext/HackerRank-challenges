2ab643a3c5d614681a83e1b295fdc19fcc47f637
djorgeext <djorge.ext@fi.uba.ar>
2026-08-01 23:33:05 -0300

    first submission

--- NLP/Medium/Stitch-the-Torn-Wiki/stitch-the-torn-wiki-testcases/solution.py
+++ NLP/Medium/Stitch-the-Torn-Wiki/stitch-the-torn-wiki-testcases/solution.py
@@ -1,34 +1,31 @@
-from nltk.tokenize import wordpunct_tokenize
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+
 def text_similarity(text1, text2):
-    vectorizer = TfidfVectorizer(
-        lowercase=True,
-        stop_words="english"
-    )
-
+    vectorizer = TfidfVectorizer(lowercase=True, stop_words="english")
     X = vectorizer.fit_transform([text1, text2])
-
     return cosine_similarity(X[0], X[1])[0, 0]
 
-text_path = 'NLP/Medium/Stitch-the-Torn-Wiki/stitch-the-torn-wiki-testcases/input/input00.txt'
 
-with open(text_path, 'r') as f:
-    text = f.read().splitlines()
+def main():
+    lines = sys.stdin.read().splitlines()
 
-separator_set = text.index("*****")
+    separator_idx = lines.index("*****")
+    a_set = lines[1:separator_idx]
+    b_set = lines[separator_idx + 1 :]
 
-a_set = text[1:separator_set]
-b_set = text[separator_set+1:]
+    for paragraph_a in a_set:
+        similarities = []
+        for paragraph_b in b_set:
+            similarities.append(text_similarity(paragraph_a, paragraph_b))
 
-matches = []
-for paragraph_a in a_set:
-    similarity = []
-    for paragraph_b in b_set:
-        similarity.append(text_similarity(paragraph_a, paragraph_b))
-    idx = similarity.index(max(similarity)) + 1
-    matches.append(idx)
+        best_idx = similarities.index(max(similarities)) + 1
+        print(best_idx)
 
 
-print(matches)
+if __name__ == "__main__":
+    main()
